482e346c2325a0e07dbe9cf8fe242cc04f4486de
aselnigmatullina <nigmatullina.asel2016@yandex.ru>
2019-09-22 18:02:04 +0300

    changed

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ console.log('worked');
 //regular purchases: 1% cashback 0-1_000
 //Increased Cashback purchases: 5% cashback 1_001-10_000
 //Special offers purchases: 30% 10_001
-const previousPurchases=1000;
 
-const regularPurchases = previousPurchases*0.01;
-const increasedCashbackPurchases = previousPurchases*0.05;
-const specialOffersPurchases = previousPurchases*0.3;
+const currentPurchases=100;
+
+const regularPurchases = currentPurchases*0.01;
+const increasedCashbackPurchases = currentPurchases*0.05;
+const specialOffersPurchases = currentPurchases*0.3;
 
 
-let bonusesPerOneThousand;
-if(previousPurchases>0 && previousPurchases<=1000){
-    bonusesPerOneThousand = regularPurchases;
-} else if(previousPurchases>10000){
-    bonusesPerOneThousand = specialOffersPurchases;
-} else{
-    bonusesPerOneThousand = increasedCashbackPurchases;
+let bonusesFromThePurchase=(currentPurchases>0 && currentPurchases<=1000)? regularPurchases:(currentPurchases>10000)? specialOffersPurchases:increasedCashbackPurchases;
+
+if(bonusesFromThePurchase>3000){
+    bonusesFromThePurchase=3000;
+    console.log("Sorry,but we can't accrue more");
 }
-console.log(bonusesPerOneThousand);
+console.log(bonusesFromThePurchase);
