47c181546adf111bc82b30b04f149e6fc5fdd488
aselnigmatullina <nigmatullina.asel2016@yandex.ru>
2019-09-24 23:49:05 +0300

    fixes #2 The ternary operator was changed to a conditional operator

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 console.log('worked');
 
-
-const currentPurchases=100;
+const currentPurchases = 10100;
 
 const regularPurchases = currentPurchases*0.01;
 const increasedCashbackPurchases = currentPurchases*0.05;
 const specialOffersPurchases = currentPurchases*0.3;
 
 
-let bonusesFromThePurchase=(currentPurchases>0 && currentPurchases<=1000)? regularPurchases:(currentPurchases>10000)? specialOffersPurchases:increasedCashbackPurchases;
+let bonusesFromThePurchase;
+if (currentPurchases > 0 && currentPurchases <= 1000) {
+    bonusesFromThePurchase = regularPurchases;
+} else if(currentPurchases > 10000) { 
+    bonusesFromThePurchase = specialOffersPurchases;
+} else {
+    bonusesFromThePurchase = increasedCashbackPurchases;
+}
 
-if(bonusesFromThePurchase>3000){
-    bonusesFromThePurchase=3000;
+if (bonusesFromThePurchase > 3000) {
+    bonusesFromThePurchase = 3000;
     console.log("Sorry,but we can't accrue more");
 }
 console.log(bonusesFromThePurchase);
