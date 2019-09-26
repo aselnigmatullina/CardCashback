a313661399421972e9192dbc6693c2c1815e3769
aselnigmatullina <nigmatullina.asel2016@yandex.ru>
2019-09-26 12:29:32 +0300

    fixes #5 Changed the name of the variable

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const increasedCashbackPurchases = currentPurchases*0.05;
 const specialOffersPurchases = currentPurchases*0.3;
 
 
-let bonusesFromThePurchase;
+let cardCashback;
 if (currentPurchases > 0 && currentPurchases <= 1000) {
-    bonusesFromThePurchase = regularPurchases;
+    cardCashback = regularPurchases;
 } else if(currentPurchases > 10000) { 
-    bonusesFromThePurchase = specialOffersPurchases;
+    cardCashback = specialOffersPurchases;
 } else {
-    bonusesFromThePurchase = increasedCashbackPurchases;
+    cardCashback = increasedCashbackPurchases;
 }
 
-if (bonusesFromThePurchase > 3000) {
-    bonusesFromThePurchase = 3000;
+if (cardCashback > 3000) {
+    cardCashback = 3000;
     console.log("Sorry,but we can't accrue more");
 }
-console.log(bonusesFromThePurchase);
+console.log(cardCashback);
